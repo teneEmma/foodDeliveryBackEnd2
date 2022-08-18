f39199c09e980b897f0569cd4b1e5a6f9db1a2b2
kodage111 <tetemjames@gmail.com>
2022-08-18 14:35:47 +0100

    validation schema adjustement commit

--- Controller/users/validation.js
+++ Controller/users/validation.js
@@ -6,7 +6,7 @@ export function createUserValidation(userToValidate){
     const joiSchema = Joi.object({
         username: Joi.string().min(4).max(15).required(),
         email: Joi.string().email({minDomainSegments: 2}).trim(true).required(),
-        password: Joi.string().trim(true),
+        password: Joi.string().min(6).max(20).trim(true),
         phoneNumber: Joi.string().length(12),
         userType: Joi.string().valid(userType[0], userType[1])
     });
@@ -26,7 +26,7 @@ export function getUserValidation(dataToValidate){
 export function authenticateUserValidation(dataToValidate){
     const joiSchema = Joi.object({
         email: Joi.string().email({ minDomainSegments: 2 }).trim(true).required(),
-        password: Joi.string().trim(true).required()
+        password: Joi.string().min(6).max(20).trim(true)
     });
     
     return joiSchema.validate(dataToValidate);
