2de9d634d6725bbfb8a13cd961a8b3da4de36665
kodage111 <tetemjames@gmail.com>
2022-08-20 23:56:14 +0100

    first commit in our branch

--- models/users/Users.js
+++ models/users/Users.js
@@ -8,14 +8,4 @@ const userSchema = mongoose.Schema({
     userType : String
 });
 
-userSchema.pre("save", async function(next){
-    console.log("*********in the pre function************");
-    const {password} = this;
-    const salt = await bcrypt.genSalt(10);
-    const secondPassword =await bcrypt.hash(password, salt);
-    console.log(`actual password: ${password}|| encrypted: ${secondPassword}`);
-
-    next();
-});
-
 export const users = mongoose.model("users",userSchema);
