2f2e4d2dc97680b1e3cd93fca4c9b05dab7d61e5
kodage111 <tetemjames@gmail.com>
2022-09-06 14:30:39 +0100

    some modifications made

---
+++ Constants/networkCodes.js
@@ -0,0 +1,15 @@
+const code = {
+    clientError: {
+        badrequest: 400,
+        unauthorized: 401,
+        forbidden: 403, 
+        notFound: 404
+    },
+    successful: {
+        ok: 200,
+        created: 201,
+        Accepted: 202
+    }
+};
+
+module.exports = code;
